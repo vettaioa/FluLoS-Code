1187e256dee5db0502bc3d644f87bd978e9ee2d5
hauptpas <hauptpas@students.zhaw.ch>
2021-04-06 16:44:41 +0200

    Added DeltaList Dummy Program

---
+++ DeltaList/DeltaListReplacer/DeltaReplacer.cs
@@ -0,0 +1,58 @@
+﻿using FuzzySearching;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeltaListReplacer
+{
+    class DeltaReplacer
+    {
+        private FuzzySearch fuzzySearch;
+        private SearchableCollection atcWords;
+        private SearchableDictionary<string> deltaList;
+
+        public DeltaReplacer(
+            IReadOnlyDictionary<string, string> phonetics,
+            IReadOnlyList<string> atcWords,
+            IReadOnlyDictionary<string, string> deltaList
+        )
+        {
+            fuzzySearch = new FuzzySearch(phonetics);
+
+            this.atcWords = new SearchableCollection(fuzzySearch, atcWords);
+            this.atcWords.Ratio = 0.6f;
+
+            this.deltaList = new SearchableDictionary<string>(fuzzySearch, deltaList);
+            this.deltaList.Ratio = 0.8f;
+        }
+
+        public string Replace(string inputStr)
+        {
+            var sb = new StringBuilder();
+            foreach(var word in inputStr?.Split(' '))
+            {
+                sb.Append(ReplaceWord(word)).Append(' ');
+            }
+            return sb.ToString();
+        }
+
+        private string ReplaceWord(string word)
+        {
+            string atcWord = atcWords.fuzzySearching(word);
+            if (atcWord != null)
+            {
+                Console.WriteLine($"[DEBUG] Replacing '{word}' with '{atcWord}'");
+                return atcWord;
+            }
+
+            string deltaWord = deltaList.fuzzySearching(word);
+            if(deltaWord != null)
+            {
+                Console.WriteLine($"[DEBUG] Replacing '{word}' with '{deltaWord}'");
+                return deltaWord;
+            }
+
+            return word; // no word alternative found => use original
+        }
+    }
+}
