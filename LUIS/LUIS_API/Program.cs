0d80ef7f0526ae421e1f1c2959d545d12c5a6dd4
Ioannis Vettas <vettaioa@students.zhaw.ch>
2021-04-19 11:19:28 +0200

    sample code für luis api call

---
+++ LUIS/LUIS_API/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+
+namespace LUIS_API
+{
+    class Program
+    {
+
+        private const string URL = "https://westeurope.api.cognitive.microsoft.com/luis/prediction/v3.0/apps/468f22b2-8753-4107-bb47-a4218efa8d62/slots/staging/predict?subscription-key=b6ecc84a0ac849819321f0c30eaf2301&verbose=true&show-all-intents=true&log=true&query=";
+        private const string QUERY = "lufthansa 3556 rhein radar identified turn right by 15 degrees and climb flight level 290";
+
+        static void Main(string[] args)
+        {
+            var request = WebRequest.Create(URL + QUERY);
+
+            using var webResponse = request.GetResponse();
+            using var webStream = webResponse.GetResponseStream();
+
+            using var reader = new StreamReader(webStream);
+            var data = reader.ReadToEnd();
+
+            Console.WriteLine(data);
+        }
+    }
+}
