80044327b26c6852baec66441b753c9cb541e2c0
Ioannis Vettas <vettaioa@students.zhaw.ch>
2021-04-22 15:24:17 +0200

    testprogram for s2t

--- SpeechToText/SpeechToText/SpeechToText/TestProgram.cs
+++ SpeechToText/SpeechToText/SpeechToText/TestProgram.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using SpeechToText.Model;
+using System;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace SpeechToText
 {
     class TestProgram
     {
-        static void Main(string[] args)
+        private const string REGION = "westeurope";
+        private const string TESTFILE = "../../../../../../data/audio/test/sm1_01_113.wav";
+        private const string CREDENTIALS_PATH = "../../../../../../flulos_credentials.json";
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.WriteLine("Setting up transcriber...");
+                SpeechTranscriber transcriber = new SpeechTranscriber(REGION, CREDENTIALS_PATH);
+
+                Console.WriteLine("Transcribing file...");
+                string[] results = await transcriber.TranscribeAudioFile(TESTFILE);
+
+                if (results != null)
+                {
+                    Console.WriteLine("Results:");
+                    foreach (string transcription in results)
+                    {
+                        Console.WriteLine("- {0}", transcription);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Unknown error occured!");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error occured: {0}", ex.Message);
+            }
         }
     }
 }
