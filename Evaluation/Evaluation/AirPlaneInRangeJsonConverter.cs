3a4369888d5a39dcd0b81e7adec35bfede7eaea0
hauptpas <hauptpas@students.zhaw.ch>
2021-05-01 23:08:56 +0200

    added JsonConverter for AirPlanesInRange call

---
+++ Evaluation/Evaluation/AirPlaneInRangeJsonConverter.cs
@@ -0,0 +1,63 @@
+﻿using Evaluation.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Evaluation
+{
+    class AirPlaneInRangeJsonConverter : JsonConverter<AirPlaneInRange>
+    {
+        public override AirPlaneInRange Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if(reader.TokenType != JsonTokenType.StartArray)
+            {
+                throw new JsonException("Expected Start of Array");
+            }
+            reader.Read(); // start of array
+
+            var planeId = reader.GetInt32();
+            var planePosition = ReadProperty<AirPlanePosition>(ref reader, typeof(AirPlanePosition), options);
+
+            if(reader.TokenType != JsonTokenType.EndArray)
+            {
+                throw new JsonException("Expected End of Array");
+            }
+
+            return new AirPlaneInRange
+            {
+                Id = planeId,
+                Position = planePosition,
+            };
+        }
+
+        public override void Write(Utf8JsonWriter writer, AirPlaneInRange value, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        //// from https://github.com/dotnet/runtime/blob/81bf79fd9aa75305e55abe2f7e9ef3f60624a3a1/src/libraries/System.Text.Json/src/System/Text/Json/Serialization/Converters/JsonValueConverterKeyValuePair.cs
+        private T ReadProperty<T>(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            T k;
+
+            //Attempt to use existing converter first before re-entering through JsonSerializer.Deserialize().
+            //The default converter for objects does not parse null objects as null, so it is not used here.
+            if (typeToConvert != typeof(object) && (options?.GetConverter(typeToConvert) is JsonConverter<T> keyConverter))
+            {
+                reader.Read(); // start of object
+                k = keyConverter.Read(ref reader, typeToConvert, options);
+                reader.Read(); // end of object
+            }
+            else
+            {
+                k = JsonSerializer.Deserialize<T>(ref reader, options);
+            }
+
+            return k;
+        }
+    }
+}
---
+++ Evaluation/Evaluation/Model/AirPlaneInRange.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluation.Model
+{
+    public class AirPlaneInRange
+    {
+        public int Id { get; set; }
+        public AirPlanePosition Position { get; set; }
+    }
+
+    public class AirPlanePosition
+    {
+        public int Altitude { get; set; }
+        public double Heading { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public bool OnGround { get; set; }
+        public string PointType { get; set; }
+        public int Speed { get; set; }
+        public string Squawk { get; set; }
+        public long Timestamp { get; set; }
+        public int VerticalRate { get; set; }
+    }
+}
