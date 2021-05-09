f19add23a2b3f5e0636799f459658a1cac08fb60
hauptpas <hauptpas@students.zhaw.ch>
2021-05-09 20:37:21 +0200

    added knownAirline

---
+++ Evaluation/Evaluation/Model/KnownAirline.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluation.Model
+{
+    class KnownAirline
+    {
+        public string Iata { get; set; }
+        public string Icao { get; set; }
+        public string Name { get; set; }
+        public string CallSign { get; set; }
+    }
+}
