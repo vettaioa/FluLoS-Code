4e89d07f10196a6a2c82c3e9faac3aa739311a80
hauptpas <hauptpas@students.zhaw.ch>
2021-05-28 13:00:49 +0200

    multiple improvements added rml vs luis comparison

--- calculation/context/utils.py
+++ calculation/context/utils.py
@@ -1,6 +1,4 @@
-"""
-Authors: Ioannis Vettas & Pascal Haupt
-"""
+import numpy as np
 
 flatten = lambda t: [item for sublist in t for item in sublist]
 
@@ -15,3 +13,29 @@ def hamming_distance(string1, string2):
         if string1[i] != string2[i]:
             distance += 1
     return distance
+
+def levenshtein_distance(seq1, seq2):
+    size_x = len(seq1) + 1
+    size_y = len(seq2) + 1
+    matrix = np.zeros ((size_x, size_y))
+    for x in range(size_x):
+        matrix [x, 0] = x
+    for y in range(size_y):
+        matrix [0, y] = y
+
+    for x in range(1, size_x):
+        for y in range(1, size_y):
+            if seq1[x-1] == seq2[y-1]:
+                matrix [x,y] = min(
+                    matrix[x-1, y] + 1,
+                    matrix[x-1, y-1],
+                    matrix[x, y-1] + 1
+                )
+            else:
+                matrix [x,y] = min(
+                    matrix[x-1,y] + 1,
+                    matrix[x-1,y-1] + 1,
+                    matrix[x,y-1] + 1
+                )
+    #print (matrix)
+    return (matrix[size_x - 1, size_y - 1])
