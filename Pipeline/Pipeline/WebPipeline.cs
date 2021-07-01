1132c1bdb7b4ea2111e7cb7fe8347a2cb1b8ecb3
hauptpas <hauptpas@students.zhaw.ch>
2021-07-01 15:42:20 +0200

    Added New WebPipeline WIP !!!

---
+++ Pipeline/Pipeline/WebPipeline.cs
@@ -0,0 +1,65 @@
+﻿using Evaluation;
+using Evaluation.Model;
+using Pipeline.WebUI;
+using SharedModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Pipeline
+{
+    class WebPipeline : Pipeline
+    {
+        private PipelineWebEndpoint webEndpoint;
+
+        public WebPipeline(AppConfiguration config): base(config)
+        {
+            Console.WriteLine("Initializing Web UI Endpoint");
+
+            var airplanes = GetRadarAirplanes().GetAwaiter().GetResult();
+            webEndpoint = new PipelineWebEndpoint(config, airplanes);
+
+            // TODO: call Run() method eg. on webEndpoint event
+        }
+
+        public void StartWebEndpoint()
+        {
+            webEndpoint.Run();
+        }
+
+        public void StopWebEndpoint()
+        {
+            webEndpoint.Stop();
+        }
+
+
+        protected override void WriteToOutput(PipelineOutputType outputType, string fileName, string jsonData)
+        {
+            webEndpoint.PipelineOutputReceived(outputType, fileName, jsonData);
+            base.WriteToOutput(outputType, fileName, jsonData); // output as file
+        }
+
+        private async Task<IEnumerable<RadarAirplane>> GetRadarAirplanes() // Workaround to get the current Airspace for the Web Demo
+        {
+            IEnumerable<RadarAirplane> radarAirplanes = null;
+            if (config.Evaluation.UseMockedAirspace)
+            {
+                // use a mocked airspace from predefined json
+                radarAirplanes = JsonSerializer.Deserialize<IEnumerable<RadarAirplane>>(File.ReadAllText(config.Evaluation.MockedAirspaceFile));
+            }
+            else
+            {
+                // call radar to get current airspace
+                var radarScanner = new RadarScanner(config.Evaluation.AirplanesInRangeUrl, config.Evaluation.AirplaneDetailsUrl);
+                radarAirplanes = await radarScanner.GetRadarAirplanes(config.Evaluation.LatitudeMinMax[0], config.Evaluation.LatitudeMinMax[1],
+                                                                          config.Evaluation.LongitudeMinMax[0], config.Evaluation.LongitudeMinMax[1]);
+            }
+
+            return radarAirplanes;
+        }
+    }
+}
--- Pipeline/Pipeline/WebUI/PipelineWebEndpoint.cs
+++ Pipeline/Pipeline/WebUI/PipelineWebEndpoint.cs
@@ -1,6 +1,8 @@
-﻿using Pipeline.Model;
+﻿using Evaluation.Model;
+using Pipeline.Model;
 using SharedModel;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -13,9 +15,6 @@ namespace Pipeline.WebUI
 {
     class PipelineWebEndpoint
     {
-        private const string REQUEST_MIME = "audio/webm;codecs=opus";
-        private const string AUDIO_FILENAME = "audio.webm";
-
         private HttpListener listener;
         private bool running = true;
 
@@ -23,7 +22,11 @@ namespace Pipeline.WebUI
         private static ContextExtractor contextExtractor;
         private static HttpListenerContext httpListenerContext;
 
-        public PipelineWebEndpoint(AppConfiguration config)
+        private IEnumerable<RadarAirplane> radarAirplanes;
+
+        private IDictionary<string, PipelineResult> pipelineResults = new ConcurrentDictionary<string, PipelineResult>();
+
+        public PipelineWebEndpoint(AppConfiguration config, IEnumerable<RadarAirplane> radarAirplanes)
         {
             speechToTextConfig = config.SpeechToText;
             speechToTextConfig.SpeechToTextMode = SpeechToTextMode.FileSingle;
@@ -32,6 +35,8 @@ namespace Pipeline.WebUI
             listener = new HttpListener();
             listener.Prefixes.Add("http://+:8080/");
             //listener.Prefixes.Add("https://*:8081/");
+
+            this.radarAirplanes = radarAirplanes;
         }
 
         public async Task Run()
@@ -39,23 +44,41 @@ namespace Pipeline.WebUI
             listener.Start();
             Console.WriteLine("Listening for HTTP requests");
 
-            while(running)
+            while (running)
             {
                 var context = await listener.GetContextAsync();
-                
-                switch(context.Request.Url.AbsolutePath.Trim('/'))
+
+                var path = context.Request.Url.AbsolutePath.Trim('/');
+                switch (path)
                 {
                     case "":
-                    case "index.html":
-                        HandleIndexPage(context);
+                        HandleStaticFile(context, "index.html");
                         break;
-                    case "process":
-                        await HandleSpeechInputAsync(context);
+                    case "index.html":
+                    case "app.js":
+                    case "audiolib.js":
+                        HandleStaticFile(context, path);
+                        break;
+                    //case "airspace":
+                    //    HandleAirspaceAsync(context);
+                    //    break;
+                    //case "process":
+                    //    HandleSpeechInputAsync(context);
+                    //    break;
+                    //case "output":
+                    //    HandleOutputAsync(context);
+                    //    break;
+                    default:
+                        context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                        context.Response.Close();
                         break;
                 }
+
+                context.Response.OutputStream.Close();
                 context.Response.Close();
+
             }
-            
+            listener.Close();
         }
 
         public void Stop()
@@ -65,12 +88,70 @@ namespace Pipeline.WebUI
             Console.WriteLine("Stopped listening for HTTP requests");
         }
 
-        private static void HandleIndexPage(HttpListenerContext context)
-        {
-            context.Response.ContentType = "text/html";
 
-            var file = File.OpenRead(@"WebUI\index.html");
-            file.CopyTo(context.Response.OutputStream);
+        public void PipelineOutputReceived(PipelineOutputType outputType, string uid, string jsonData)
+        {
+            if (!pipelineResults.ContainsKey(uid))
+            {
+                pipelineResults[uid] = new PipelineResult { uid = uid };
+            }
+
+            var result = pipelineResults[uid];
+            switch (outputType)
+            {
+                case PipelineOutputType.TRANSCRIPTIONS:
+                    result.transcriptionsJson = jsonData;
+                    break;
+                case PipelineOutputType.CONTEXTS:
+                    result.contextsJson = jsonData;
+                    break;
+                case PipelineOutputType.EVALUATIONFLAGS:
+                    result.evaluationflagsJson = jsonData;
+                    break;
+                case PipelineOutputType.VALIDATEDMERGED:
+                    result.evaluationflagsJson = jsonData;
+                    break;
+            }
+        }
+
+        private static void HandleStaticFile(HttpListenerContext context, string fileName)
+        {
+            string path = $"WebUI\\{fileName}";
+
+            string mimeType = "text/plain";
+            switch (Path.GetExtension(path))
+            {
+                case ".html":
+                    mimeType = "text/html";
+                    break;
+                case ".js":
+                    mimeType = "application/javascript";
+                    break;
+                case ".css":
+                    mimeType = "text/css";
+                    break;
+            }
+            context.Response.ContentType = mimeType;
+            try
+            {
+                var file = File.OpenRead(path);
+                file.CopyTo(context.Response.OutputStream);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed to read Static File", e);
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            }
+            
+            context.Response.Close();
+        }
+
+        private async Task HandleAirspaceAsync(HttpListenerContext context)
+        {
+            context.Response.ContentType = "application/json";
+            await JsonSerializer.SerializeAsync(context.Response.OutputStream, new { airplanes = radarAirplanes });
+
+            context.Response.Close();
         }
 
         private static async Task HandleSpeechInputAsync(HttpListenerContext context)
@@ -79,38 +160,46 @@ namespace Pipeline.WebUI
             if (context.Request != null)
             {
                 Console.WriteLine("Request received");
-                if(context.Request.ContentType != null && context.Request.ContentType == REQUEST_MIME)
-                {
-                    Console.WriteLine("Processing audio...");
-                    bool audioSaved = false;
-                    try
-                    {
-                        using (var file = File.OpenWrite(AUDIO_FILENAME))
-                        using (var stream = context.Request.InputStream)
-                        {
-                            stream.CopyTo(file);
-                        }
-                        audioSaved = true;
-                    }
-                    catch { }
+                var uid = Guid.NewGuid().ToString();
 
-                    if (audioSaved)
-                    {
-                        Console.WriteLine("Running Speech to Text...");
-                        speechToTextConfig.InputAudioFile = AUDIO_FILENAME;
-                        SpeechToTextRunner speechToText = new SpeechToTextRunner(speechToTextConfig);
-                        speechToText.SpeechTranscribed += ProcessTranscriptions;
-                        await speechToText.Run();
-                    }
-                    else
-                    {
-                        Console.WriteLine("Error saving audio to file!");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Wrong format, must be {0}", REQUEST_MIME);
-                }
+                // TODO: copy demo audio with new file name "uid.wav" or use other audio source
+
+                //if(context.Request.ContentType != null && context.Request.ContentType == REQUEST_MIME)
+                //{
+                //    Console.WriteLine("Processing audio...");
+                //    bool audioSaved = false;
+                //    try
+                //    {
+                //        using (var file = File.OpenWrite(AUDIO_FILENAME))
+                //        using (var stream = context.Request.InputStream)
+                //        {
+                //            stream.CopyTo(file);
+                //        }
+                //        audioSaved = true;
+                //    }
+                //    catch { }
+
+                //    if (audioSaved)
+                //    {
+                //        Console.WriteLine("Running Speech to Text...");
+                //        speechToTextConfig.InputAudioFile = AUDIO_FILENAME;
+                //        SpeechToTextRunner speechToText = new SpeechToTextRunner(speechToTextConfig);
+                //        speechToText.SpeechTranscribed += ProcessTranscriptions;
+                //        await speechToText.Run();
+                //    }
+                //    else
+                //    {
+                //        Console.WriteLine("Error saving audio to file!");
+                //    }
+                //}
+                //else
+                //{
+                //    Console.WriteLine("Wrong format, must be {0}", REQUEST_MIME);
+                //}
+
+                byte[] buffUid = Encoding.UTF8.GetBytes(uid);
+                context.Response.ContentType = "text/plain";
+                context.Response.OutputStream.Write(buffUid, 0, buffUid.Length);
             }
 
             // context.Request.InputStream
@@ -119,7 +208,30 @@ namespace Pipeline.WebUI
             // the following example code produces a working .webm audio file
             //var file = File.OpenWrite(@"audio.webm");
             //context.Request.InputStream.CopyTo(file);
+
+
+            context.Response.Close();
         }
+
+        private async Task HandleOutputAsync(HttpListenerContext context)
+        {
+            string uid = context.Request.QueryString["uid"];
+            string type = context.Request.QueryString["type"];
+
+            PipelineResult result = pipelineResults[uid];
+
+            // TODO: WAIT until the result's json is not null
+
+            //switch (type)
+            //{
+            //    case "transcriptions":
+            //        // result.transcriptionsJson
+            //    default:
+            //        break;
+            //}
+            context.Response.Close();
+        }
+
 
         private static void ProcessTranscriptions(TranscriptionResult transcriptionResult)
         {
@@ -135,5 +247,14 @@ namespace Pipeline.WebUI
                 writer.Write("No results found");
             }
         }
+    }
+
+    internal class PipelineResult
+    {
+        public string uid { get; set; }
+        public string transcriptionsJson { get; set; }
+        public string contextsJson { get; set; }
+        public string evaluationflagsJson { get; set; }
+        public string validatedmergedJson { get; set; }
     }
 }
